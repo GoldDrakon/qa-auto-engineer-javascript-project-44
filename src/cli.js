82cdcda8b71ce7f9753320b1a359e9437222e4da
Ilya Ermakov <Ermakov_Ilia88@mail.ru>
2025-04-13 15:25:01 +0700

    update

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-var name;
+export let name;
 const helloName = () => {
   console.log('Welcome to the Brain Games!');
   name = readlineSync.question('May I have your name? ');
@@ -8,4 +8,3 @@ const helloName = () => {
 };
 
 export { helloName };
-export { name };
